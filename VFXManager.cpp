cbb095498571b7dc5287b171cd2011e591319419
海斗 矢吹 <220177@jc-21.jp>
2024-02-05 11:42:03 +0900

    パーティクル完全体完成

--- VFXManager.cpp
+++ VFXManager.cpp
@@ -7,40 +7,41 @@ namespace VFXManager {
 
 	void Initialize()
 	{
-		hit.textureFileName = "circle_Y.png";
-		hit.position = XMFLOAT3();
-		hit.delay = 0;
-		hit.number = 10;
-		hit.lifeTime = 60;
-		hit.positionRnd = XMFLOAT3(0.5, 0, 0.5);
-		hit.direction = XMFLOAT3(0, 0, 1);
-		hit.directionRnd = XMFLOAT3(90, 90, 90);
-		hit.speed = 0.3f;
-		hit.speedRnd = 0;
-		hit.accel = 0.93;
-		hit.size = XMFLOAT2(0.1, 0.1);
-		hit.sizeRnd = XMFLOAT2(0.4, 0.4);
-		hit.color = XMFLOAT4(0, 0, 1, 1);
-		hit.deltaColor = XMFLOAT4(0, 1, 1, 0);
-
-		//ìñÇΩÇ¡ÇΩéûÇÃVFX
-		snow.textureFileName = "defaultParticle.png";
+		//ê·ã 
+		snow.textureFileName = "circle_Y.png";
 		snow.position = XMFLOAT3();
 		snow.delay = 0;
-		snow.number = 40;
-		snow.lifeTime = 30;
+		snow.number = 10;
+		snow.lifeTime = 60;
 		snow.positionRnd = XMFLOAT3(0.5, 0, 0.5);
-		snow.direction = XMFLOAT3(0, 1, 0);
+		snow.direction = XMFLOAT3(0, 0, 1);
 		snow.directionRnd = XMFLOAT3(90, 90, 90);
-		snow.speed = 0.25f;
-		snow.speedRnd = 1;
+		snow.speed = 0.3f;
+		snow.speedRnd = 0;
 		snow.accel = 0.93;
-		snow.size = XMFLOAT2(0.4, 0.4);
+		snow.size = XMFLOAT2(0.1, 0.1);
 		snow.sizeRnd = XMFLOAT2(0.4, 0.4);
-		snow.scale = XMFLOAT2(0.99, 0.99);
-		snow.color = XMFLOAT4(1, 1, 1, 1);
-		snow.deltaColor = XMFLOAT4(0, 0, 0, 0);
-		snow.gravity = 0.003f;
+		snow.color = XMFLOAT4(0, 0, 1, 1);
+		snow.deltaColor = XMFLOAT4(0, 1, 1, 0);
+
+		//ìñÇΩÇ¡ÇΩéûÇÃVFX
+		hit.textureFileName = "defaultParticle.png";
+		hit.position = XMFLOAT3();
+		hit.delay = 0;
+		hit.number = 40;
+		hit.lifeTime = 30;
+		hit.positionRnd = XMFLOAT3(0.5, 0, 0.5);
+		hit.direction = XMFLOAT3(0, 1, 0);
+		hit.directionRnd = XMFLOAT3(90, 90, 90);
+		hit.speed = 0.25f;
+		hit.speedRnd = 1;
+		hit.accel = 0.93;
+		hit.size = XMFLOAT2(0.4, 0.4);
+		hit.sizeRnd = XMFLOAT2(0.4, 0.4);
+		hit.scale = XMFLOAT2(0.99, 0.99);
+		hit.color = XMFLOAT4(1, 1, 1, 1);
+		hit.deltaColor = XMFLOAT4(0, 0, 0, 0);
+		hit.gravity = 0.003f;
 	}
 
 }
